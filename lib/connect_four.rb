21cc244868f5bd5105e570b74fe03dec92187f83
koka <kiraoverton27@gmail.com>
2021-08-12 14:23:57 -0400

    winning algorithms with tests

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,58 +1,157 @@
 # frozen_string_literal: true
 
+require 'matrix'
+
 class ConnectFour
+  # six rows by seven columns
   def initialize(board = Array.new(6) { Array.new(7) })
     @board = board
   end
 
-  # the horizontal pattern is always start -> start + 4
-  # instead of writing out that array combinations maybe determine if it fits the pattern?
-  #
-  HORIZONTAL_WINS = [
-    [0, 1, 2, 3],
-    [6, 7, 8, 9],
-    [12, 13, 14, 15],
-    [18, 19, 20, 21],
-    [24, 25, 26, 27],
-    [30, 31, 32, 33],
-    [36, 37, 38, 39],
-    [1, 2, 3, 4],
-    [7, 8, 9, 10],
-    [13, 14, 15, 16],
-    [19, 20, 21, 22],
-    [25, 26, 27, 28],
-    [31, 32, 33, 34],
-    [37, 38, 39, 40],
-    [2, 3, 4, 5],
-    [8, 9, 10, 11],
-    [14, 15, 16, 17],
-    [20, 21, 22, 23],
-    [26, 27, 28, 29],
-    [32, 33, 34, 35],
-    [38, 39, 40, 41]
-  ].freeze
+  DIAGONAL_PATTERNS = [[1, -1],
+                       [1, 1],
+                       [-1, -1],
+                       [-1, 1]].freeze
 
-  VERTICAL_WINS = [
-    [0, 6, 12, 18, 24, 30, 36],
-    [1, 7, 13, 19, 25, 31, 37],
-    [2, 8, 14, 20, 26, 32, 38],
-    [3, 9, 15, 21, 27, 33, 39],
-    [4, 10, 16, 22, 28, 34, 40],
-    [5, 11, 17, 23, 29, 35, 41]
-  ].freeze
+  # [[nil, nil, nil, nil, nil, 'X', nil],
+  #             [nil, nil, nil, nil, nil, 'X', nil],
+  #             [nil, nil, nil, nil, nil, 'X', nil],
+  #             [nil, nil, nil, nil, nil, 'X', nil],
+  #             [nil, nil, nil, nil, nil, nil, nil],
+  #             [nil, nil, nil, nil, nil, nil, nil]]
+
+  # [[nil, nil, nil, nil, nil, nil, nil],
+  #            [nil, nil, nil, nil, nil, nil, nil],
+  #            [nil, nil, nil, nil, nil, nil, nil],
+  #            [nil, nil, nil, nil, nil, nil, nil],
+  #            [nil, nil, nil, nil, nil, nil, nil],
+  #            ['X', 'X', 'X', 'X', nil, nil, nil]]
+
+  # [[nil, nil, nil, nil, nil, nil, nil],
+  #             [nil, 'X', nil, nil, nil, nil, nil],
+  #             [nil, nil, 'X', nil, nil, nil, nil],
+  #             [nil, nil, nil, 'X', nil, nil, nil],
+  #             [nil, nil, nil, nil, 'X', nil, nil],
+  #             [nil, nil, nil, nil, nil, nil, nil]]
 
   def return_board
-    p @board
+    # p @board
+    @board.map do |line|
+      p line
+    end
     @board
   end
 
+  # count the number of 'X' in each row
+  def horizontal_wins?
+    win = false
+    @board.map do |line|
+      win = check_row(line)
+      return true if win
+    end
+
+    false
+  end
+
+  # checking one line of the grid, go square by square
+  def check_row(line)
+    counter = 0
+    line.map do |square|
+      return true if counter == 3
+
+      if square == 'X' # @player
+        counter += 1
+
+      elsif square != 'X'
+        counter = 0
+      end
+    end
+
+    false
+  end
+
+  # ex: first item in the line is 'X', continues for four lines
+  def vertical_wins?
+    i = 0
+    until i > 6
+      return true if check_column?(i)
+
+      i += 1
+    end
+    false
+  end
+
+  def check_column?(index)
+    counter = 0
+    @board.map do |line|
+      return true if counter == 3
+
+      if line[index] == 'X' # or @player
+        counter += 1
+      elsif line[index] != 'X'
+        counter = 0
+      end
+    end
+    false
+  end
+
+  # if the index matches the pattern *4 then we know it's a match
+  def diagonal_wins?
+    positions = []
+    @board.map do |line|
+      positions.push(check_player_location(line)) if line.include?('X') # or @player
+    end
+
+    return true if check_diagonal_pattern(positions)
+
+    false
+  end
+
+  def check_diagonal_pattern(position)
+    index = 0
+    counter = 0
+    DIAGONAL_PATTERNS.map do |prediction|
+      next_position = position[index] # <-- starting point
+      while position.include?(next_position)
+        next_position = []
+        next_position.push(position[index][0] + prediction[0]) # <--the first number at pos[0]
+        next_position.push(position[index][1] + prediction[1]) # <-- the second number at pos[0]
+        counter += increment_counter(position, next_position)
+
+        return true if counter == 3
+      end
+      index += 1
+    end
+    false
+  end
+
+  def increment_counter(arr, curr_position)
+    if !arr.include?(curr_position)
+      0
+    else
+      1
+    end
+  end
+
+  # find all of the indexes of a player's tic
+  def check_player_location(line)
+    arr = []
+    arr.push(@board.index(line))
+    arr.push(line.index('X'))
+    arr
+  end
+
   def check_winner?
-    # index?
-    # @board.index(@player)
-    @board.index('X')
-    return true if HORIZONTAL_WINS.any? { |line| line.all? { |square| @board[square] == 'X' } }
-    return true if VERTICAL_WINS.any? { |line| line.all? { |square| @board[square] == 'X' } }
+    return true if horizontal_wins?
+    return true if vertical_wins?
+    return true if diagonal_wins?
 
     false
   end
 end
+
+# game = ConnectFour.new
+# game.return_board
+# p game.diagonal_wins?
+# game.horizontal_wins?
+# game.vertical_wins?
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -5,36 +5,155 @@ require_relative '../lib/connect_four'
 # build connect four using test driven development
 
 # instantiating the class creates an array
-describe 'create a game board' do
-  # 7 columns by 6 rows
-  let(:build_board) { ConnectFour.new }
-  it 'returns an array of 6 rows by 7 columns' do
-    expect(build_board.return_board.length).to eq(42)
-  end
-end
+# describe 'create a game board' do
+#   # 7 columns by 6 rows
+#   let(:build_board) { ConnectFour.new }
+#   it 'returns an array of 6 rows by 7 columns' do
+#     expect(build_board.return_board.length).to eq(42)
+#   end
+# end
 
 # determine the winner
-describe 'check for horizontal wins' do
-  context 'check for horizontal wins' do
-    board = %w[X X X X]
-    let(:horizontal_row) { ConnectFour.new(board) }
+describe 'check for a horizontal win' do
+  context 'check for a horizontal win' do
+    board = [[nil, nil, nil, nil, nil, nil, nil],
+             [nil, nil, nil, nil, nil, nil, nil],
+             [nil, nil, nil, nil, nil, nil, nil],
+             [nil, nil, nil, nil, nil, nil, nil],
+             ['X', 'X', 'X', 'X', nil, nil, nil],
+             [nil, nil, nil, nil, nil, nil, nil]]
+    let(:horizontal_win) { ConnectFour.new(board) }
     it 'returns true' do
-      expect(horizontal_row.check_winner?).to eq(true)
+      expect(horizontal_win.horizontal_wins?).to eq(true)
     end
   end
-  #   context 'check for vertical wins' do
-  #     board = Array.new(42)
-  #     board[0] = 'X'
-  #     board[6] = 'X'
-  #     board[12] = 'X'
-  #     board[18] = 'X'
-  #     let(:vertical_row) { ConnectFour.new(board) }
-  #     xit 'returns true' do
-  #       expect(vertical_row.check_winner?).to eq(true)
-  #     end
-  #   end
+  context 'check for a horizontal win' do
+    board = [[nil, nil, nil, nil, nil, nil, nil],
+             [nil, nil, nil, nil, nil, nil, nil],
+             [nil, nil, 'X', 'X', 'X', 'X', nil],
+             [nil, nil, nil, nil, nil, nil, nil],
+             ['O', 'O', 'O', 'O', nil, nil, nil],
+             [nil, nil, nil, nil, nil, nil, nil]]
+    let(:horizontal_win) { ConnectFour.new(board) }
+    it 'returns true' do
+      expect(horizontal_win.horizontal_wins?).to eq(true)
+    end
+  end
+  context 'check for no horizontal win' do
+    let(:horizontal_win) { ConnectFour.new }
+    it 'returns false' do
+      expect(horizontal_win.horizontal_wins?).not_to eq(true)
+    end
+  end
 end
 
-# describe 'check for vertical wins' do
-#   context ''
-# end
+describe 'check for a vertical win' do
+  context 'check for a vertical win' do
+    vertical_board = [[nil, nil, nil, nil, nil, 'O', nil],
+                      [nil, nil, nil, nil, nil, 'O', nil],
+                      [nil, 'X', nil, nil, nil, 'O', nil],
+                      [nil, 'X', nil, nil, nil, 'O', nil],
+                      [nil, 'X', nil, nil, nil, nil, nil],
+                      [nil, 'X', nil, nil, nil, nil, nil]]
+    let(:vertical_win) { ConnectFour.new(vertical_board) }
+    it 'returns true' do
+      expect(vertical_win.vertical_wins?).to eq(true)
+    end
+  end
+  context 'check for a vertical win' do
+    vertical_board = [[nil, nil, nil, nil, nil, 'X', nil],
+                      [nil, nil, nil, nil, nil, 'X', nil],
+                      [nil, nil, nil, nil, nil, 'X', nil],
+                      [nil, nil, nil, nil, nil, 'X', nil],
+                      [nil, nil, nil, nil, nil, nil, nil],
+                      [nil, nil, nil, nil, nil, nil, nil]]
+    let(:vertical_win) { ConnectFour.new(vertical_board) }
+    it 'returns true' do
+      expect(vertical_win.vertical_wins?).to eq(true)
+    end
+  end
+  context 'check for no vertical win' do
+    let(:vertical_win) { ConnectFour.new }
+    it 'returns true' do
+      expect(vertical_win.vertical_wins?).not_to eq(true)
+    end
+  end
+end
+
+describe 'check for a diagonal win' do
+  context 'check for a right diagonal win' do
+    diagonal_board = [[nil, nil, nil, nil, nil, nil, nil],
+                      [nil, nil, nil, nil, nil, nil, nil],
+                      [nil, nil, nil, 'X', nil, nil, nil],
+                      [nil, nil, 'X', nil, nil, nil, nil],
+                      [nil, 'X', nil, nil, nil, nil, nil],
+                      ['X', nil, nil, nil, nil, nil, nil]]
+    let(:diagonal_win) { ConnectFour.new(diagonal_board) }
+    it 'returns true' do
+      expect(diagonal_win.diagonal_wins?).to eq(true)
+    end
+  end
+  context 'check for a left diagonal win' do
+    diagonal_board = [[nil, nil, nil, nil, nil, nil, nil],
+                      [nil, 'X', nil, nil, nil, nil, nil],
+                      [nil, nil, 'X', nil, nil, nil, nil],
+                      [nil, nil, nil, 'X', nil, nil, nil],
+                      [nil, nil, nil, nil, 'X', nil, nil],
+                      [nil, nil, nil, nil, nil, nil, nil]]
+    let(:diagonal_win) { ConnectFour.new(diagonal_board) }
+    it 'returns true' do
+      expect(diagonal_win.diagonal_wins?).to eq(true)
+    end
+  end
+  context 'check for no diagonal win' do
+    let(:diagonal_win) { ConnectFour.new }
+    it 'returns true' do
+      expect(diagonal_win.diagonal_wins?).not_to eq(true)
+    end
+  end
+end
+
+describe 'check for any win using check_winner?' do
+  context 'check for any win using check_winner?' do
+    let(:no_win) { ConnectFour.new }
+    it 'returns false' do
+      expect(no_win.check_winner?).to eq(false)
+    end
+  end
+  context 'check for a horizontal win' do
+    horizontal_board = [[nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
+                        ['X', 'X', 'X', 'X', nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil]]
+    let(:horizontal_win) { ConnectFour.new(horizontal_board) }
+    it 'returns true' do
+      expect(horizontal_win.check_winner?).to eq(true)
+    end
+  end
+  context 'check for a vertical win' do
+    vertical_board = [[nil, nil, nil, nil, nil, 'X', nil],
+                      [nil, nil, nil, nil, nil, 'X', nil],
+                      [nil, nil, nil, nil, nil, 'X', nil],
+                      [nil, nil, nil, nil, nil, 'X', nil],
+                      [nil, nil, nil, nil, nil, nil, nil],
+                      [nil, nil, nil, nil, nil, nil, nil]]
+    let(:vertical_win) { ConnectFour.new(vertical_board) }
+    it 'returns true' do
+      expect(vertical_win.check_winner?).to eq(true)
+    end
+  end
+  context 'check for a diagonal win' do
+    diagonal_board =   [[nil, nil, nil, nil, nil, nil, nil],
+                        [nil, 'X', nil, nil, nil, nil, nil],
+                        [nil, nil, 'X', nil, nil, nil, nil],
+                        [nil, nil, nil, 'X', nil, nil, nil],
+                        [nil, nil, nil, nil, 'X', nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil]]
+    let(:diagonal_win) { ConnectFour.new(diagonal_board) }
+    it 'returns true' do
+      expect(diagonal_win.check_winner?).to eq(true)
+    end
+  end
+end
